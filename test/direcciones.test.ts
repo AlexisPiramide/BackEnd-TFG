b50960bc2f116f2a48ebf1d6cfd6d59629df848c
AlexisPiramide <220240@fppiramide.com>
2025-03-22 14:32:54 +0100

    Test

---
+++ test/direcciones.test.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+import app from "./../src/app";
+
+describe("Direcciones Tests", () => {
+
+    it("should create a new direccion", async () => {
+        const usuario = "XXXX-XXXX-XXXX";
+        const response = await request(app)
+        .post("/direcciones/" + usuario)
+        .send({
+            calle: "Calle Falsa",
+            numero: "123",
+            codigoPostal: "54321",
+            localidad: "Shelbyville",
+            provincia: "Illinois",
+            pais: "USA"
+        });
+    
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+        expect(response.body.calle).toBe("Calle Falsa");
+        expect(response.body.numero).toBe("123");
+        expect(response.body.codigoPostal).toBe("54321");
+        expect(response.body.localidad).toBe("Shelbyville");
+        expect(response.body.provincia).toBe("Illinois");
+        expect(response.body.pais).toBe("USA");
+    });
+
+    it("GET /direcciones/:usuario - Should retrieve the created direccion", async () => {
+        const usuario = "XXXX-XXXX-XXXX";
+        const direccionId =await postDireccion(usuario);
+
+        const response = await request(app).get(`/direcciones/${direccionId}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("_id", direccionId);
+        expect(response.body.calle).toBe("Calle Falsa");
+    });
+});
+
+
+const postDireccion =async (usuario: string) => {
+
+    const response = await request(app)
+        .post("/direcciones/" + usuario)
+        .send({
+            calle: "Calle Falsa",
+            numero: "123",
+            codigoPostal: "54321",
+            localidad: "Shelbyville",
+            provincia: "Illinois",
+            pais: "USA"
+        });
+
+        return response.body.id;
+}
+
+
--- test/paquetes.test.ts
+++ test/paquetes.test.ts
@@ -1,21 +1,72 @@
 import request from "supertest";
 import app from "./../src/app";
 
+describe("Paquetes Tests", () => {
 
-describe(" Paquetes Tests", () => {
+    it("should create a new paquete", async () => {
+        const remitente = "XXXX-XXXX-XXXX";
+        const direccion_remitente = "asfasfawfafafa123";
+        const destinatario = "Maria Gómez";
 
-    it("Post /paquetes", async () => {
         const response = await request(app)
         .post("/paquetes")
         .send({
-            "_id": {
-              "$oid": "67bef9905a2641523414df2f"
+            dimensiones: "Extra Pequeño",
+            remitente: remitente,
+            direccion_remitente: direccion_remitente,
+            destinatario: destinatario,
+            direccion_destinatario: {
+            calle: "Calle Falsa",
+            numero: "123",
+            codigoPostal: "54321",
+            localidad: "Shelbyville",
+            provincia: "Illinois",
+            pais: "USA"
             },
-            "nombre": "Extra Pequeño"
-          })
+            peso: 1.5
+        });
 
         expect(response.status).toBe(201);
-        expect(response.body.id).toBeDefined();
+        expect(response.body).toHaveProperty("id");
+        expect(response.body.dimensiones).toBe("Extra Pequeño");
+        expect(response.body.remitente).toBe(remitente);
+        expect(response.body.direccion_remitente).toBe(direccion_remitente);
+        expect(response.body.destinatario).toBe(destinatario);
+    });
+
+
+    it("GET /paquetes/:id - Should retrieve the created paquete", async () => {
+
+        const paqueteId = postPaquete({
+
+        })
+
+        const response = await request(app).get(`/paquetes/${paqueteId}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("_id", paqueteId);
         expect(response.body.dimensiones).toBe("Extra Pequeño");
     });
+
+    it("GET /paquetes/gencode/:id - Should generate a barcode image", async () => {
+
+        const paqueteId = postPaquete({
+
+        })
+
+        const response = await request(app).get(`/paquetes/gencode/${paqueteId}`);
+
+        expect(response.status).toBe(200);
+        expect(response.headers["content-type"]).toBe("image/png");
+    });
+
 });
+
+const postPaquete = async (pedido) => {
+    const response = await request(app)
+        .post("/paquetes")
+        .send(pedido);
+
+    
+    return response.id;
+}
